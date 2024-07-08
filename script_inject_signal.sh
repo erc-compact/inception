af100bbf3744715381dceb209bb9feb1946c315a
RobertSenzel <86438156+RobertSenzel@users.noreply.github.com>
2024-07-08 19:40:40 +0200

    Update script_inject_signal.sh

--- script_inject_signal.sh
+++ script_inject_signal.sh
@@ -8,7 +8,7 @@
 
 fb_name="controlfb.fil"
 fb_path="/hercules/scratch/rsenzel/signal_inject/$fb_name"
-par_path="/u/rsenzel/pulsar_inject/test1.pulsar"
+par_path="/u/rsenzel/pulsar_inject/example.pulsar"
 
 sing_img="/hercules/:/hercules/ /hercules/scratch/vishnu/singularity_images/pulsar-miner_turing-sm75.sif"
 tmp_in="/tmp/rsenzel/input_data"
