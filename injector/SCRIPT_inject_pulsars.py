83e1d1515ee7b7f5fa891a47050ea79d8f416d75
RobertSenzel <86438156+RobertSenzel@users.noreply.github.com>
2024-08-19 11:12:36 +0200

    Update SCRIPT_inject_pulsars.py

--- injector/SCRIPT_inject_pulsars.py
+++ injector/SCRIPT_inject_pulsars.py
@@ -8,10 +8,13 @@ from injector.signal_injector import InjectSignal
 if __name__=='__main__':
     try:
         opts, args = getopt.getopt(sys.argv[1:], "h", ["signal=", "filterbank=", "ephem=", "output=", 'ncpu='])
-    except:
-        sys.exit(sys.exc_info()[0])
+    except Exception as err:
+        sys.exit(err)
 
-    ad = dict(opts)    
+    ad = dict(opts) 
+    if not ad.get('-h', 1):
+        sys.exit('If you need help come to my office (E0.04). :D') 
+        
     setup = SetupManager(ad['--signal'], ad['--filterbank'], ad['--ephem'],  ad['--output'])
    
     injector = InjectSignal(setup, ad['--ncpu'])
