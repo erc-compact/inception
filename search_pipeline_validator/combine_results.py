2cbc91f1d25db23eff39357d3cf2d0013d435365
RobertSenzel <robzsenz@gmail.com>
2025-11-29 17:12:49 +0100

    new sifter

--- search_pipeline_validator/combine_results.py
+++ search_pipeline_validator/combine_results.py
@@ -16,7 +16,7 @@ keys = ['inj_dir', 'fb', 'psr_ID',
         'inj_p0', 'inj_DM', 'inj_SNR', 'inj_DC', 'inj_s_index', 'inj_phase', 'inj_bin_p0', 'inj_M2', 'inj_inc', 'inj_ecc', 'inj_AoP', 'inj_T0',
         'par_p0', 'par_dm', 'par_acc', 'par_SNR',
         'pea_p0', 'pea_dm', 'pea_acc', 'pea_SNR', 'pea_cand_ID', 'pea_nbins', 'pea_drift', 'pea_harmonic',
-        'fold_p0', 'fold_dm', 'fold_acc', 'fold_SNR']
+        'fold_p0', 'fold_dm', 'fold_acc', 'fold_SNR', 'PICS_fscore', 'PICS_recall']
 
 for i, inj_dir in enumerate(inj_directories):
 
@@ -36,10 +36,17 @@ for i, inj_dir in enumerate(inj_directories):
     merged_cands = pd.read_csv(merged_cand_path[0])
 
     cand_paths = glob.glob(f'{inj_dir}/inj_cands/*_candfold.csv')
+
+    pics_path = glob.glob(f'{inj_dir}/inj_cands/pics_scores.txt')
     if cand_paths:
         cand_fold = pd.read_csv(cand_paths[0], index_col=0)
+        if pics_path:
+            pics_vals = pd.read_csv(pics_path[0])
+        else:
+            pics_vals = []
     else:
         cand_fold = []
+        pics_vals = []
 
     for psr in report:
         inj_res = [inj_number, fb_name, psr['ID'], psr['PX'][0], psr['DM'], psr['SNR'], psr['duty_cycle'], psr['spectral_index'], psr['phase_offset']]
@@ -76,13 +83,22 @@ for i, inj_dir in enumerate(inj_directories):
                 inj_res.append(psr_par['dm'])
                 inj_res.append(psr_par['acc'])
                 inj_res.append(psr_par['SNR'])
+
+                if len(pics_vals):
+                    psr_pics = pics_vals.iloc[d_pea['index_number']]
+                    inj_res.append(psr_pics[pics_vals.columns[1]])
+                    inj_res.append(psr_pics[pics_vals.columns[2]])
+                else:
+                    inj_res.append(0)
+                    inj_res.append(0)
+
             else:
                 print('no cands!')
                 fault += 1
-                inj_res.extend(np.zeros(4).tolist())
+                inj_res.extend(np.zeros(6).tolist())
 
         else:
-            inj_res.extend(np.zeros(12).tolist())
+            inj_res.extend(np.zeros(14).tolist())
 
         results.append(inj_res)
 
